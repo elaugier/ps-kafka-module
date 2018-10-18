92f1604e90fac7123643100c0d1126a44740c8c4
Eric Laugier <ela@egen.guru>
2018-10-18 07:50:13 +0200

    not finalized

--- ps-kafka-module/Send-Message-ToKafka.cs
+++ ps-kafka-module/Send-Message-ToKafka.cs
@@ -11,15 +11,26 @@ namespace ps_kafka_module
         [Parameter(Mandatory =true)]
         public string Message { get; set; }
 
+        [Parameter(Mandatory = true)]
+        public string Topic { get; set; }
+
         [Parameter(Mandatory = false)]
         public string BootstrapServers { get; set; } = "localhost:9092";
 
-        protected override void ProcessRecord()
+        protected override async void ProcessRecord()
         {
             var config = new List<KeyValuePair<String, Object>>();
             config.Add(new KeyValuePair<string, object>("BoostrapServers", BootstrapServers));
             var KafkaProducer = new Producer(config: config);
-            WriteObject(Message);
+            try
+            {
+                var dr = await KafkaProducer.ProduceAsync(Topic, new Message<Null, string> { Value = Message });
+                Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+            }
+            catch (KafkaException e)
+            {
+                Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+            }
         }
     }
 }
